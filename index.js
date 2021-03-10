c7afad010e19799708d0f308e677ffbfbbb36923
Lomba8 <jack.james.lomba@gmail.com>
2021-03-10 15:13:15 +0100

    changes to work with new covid orari

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const Jimp = require('jimp');
-const fs = require('fs')
+const fs = require('fs');
+const { log } = require('console');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 
@@ -11,7 +12,7 @@ var links = {};
 var orariJson = {};
 
 
-var materie = {
+const materie = {
     'FILOSOFIA': 'FILO',
     'SCIENZEs': 'SCI',
     'FISICA': 'FIS',
@@ -28,7 +29,7 @@ var materie = {
 
 }
 
-var colors = {
+const colors = {
     3698908415: 'STO', //    0xFFDC78DC , { r: 220, g: 120, b: 220, a: 255 }
     2027727615: 'ARTE', //  0xFF78DCAA , { r: 120, g: 220, b: 170, a: 255 }
     3698882815: 'SCI', // 0xFFDC7878, { r: 220, g: 120, b: 120, a: 255 }
@@ -47,26 +48,7 @@ var colors = {
 
 
 var OrariUrl = `https://www.messedaglia.edu.it/images/stories/${anno_link}/orario/classi.html`
-
-
-async function crop(image, ora, giorno, name) {
-
-
-    try {
-        // console.log('ora: ' + ora);
-        // console.log('giorno: ' + giorno);
-        let newImage = await Jimp.read(image)
-
-        newImage = await newImage.crop(62 + 118 * ora, 75 + 78 * giorno, 115, 75)
-            // newImage.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-            //     fs.writeFileSync('test/' + `${62 + 118 * ora}-${75 + 78 * giorno} ` + name, buffer)
-            // })
-        return newImage;
-    } catch (error) {
-        throw ('ERRORE IN CROP(): ' + error)
-    }
-
-}
+var OrariUrl_febbraio = `https://www.messedaglia.edu.it/images/stories/${anno_link}/orariofebbraio/classi.html`
 
 
 
@@ -87,7 +69,7 @@ async function getLinks() {
         ignoreHTTPSErrors: true,
     });
     const page = await browser.newPage();
-    await page.goto(OrariUrl);
+    await page.goto(OrariUrl_febbraio);
     // await page.waitForTimeout(1000);
     // await page.click('#bandeauRessource > table > tbody > tr > td:nth-child(2) > select')
     // await page.waitForTimeout(500)
@@ -99,7 +81,7 @@ async function getLinks() {
     for (let i = 1; i <= 5; i++) {
         for (let j = 0; j, j < 15; j++) {
             let classe = i.toString() + String.fromCharCode(97 + j).toUpperCase();
-            // console.log(classe);
+            console.log(classe);
 
             try {
                 const option = (await page.$x(
@@ -159,7 +141,8 @@ async function DownloadIMages() {
     let links_from_file = await getLinks()
 
     for (let classe in links_from_file) {
-        await page.goto('https://www.messedaglia.edu.it/images/stories/2020-21/orario/' + links_from_file[classe]);
+        console.log('downloading:' + classe);
+        await page.goto(OrariUrl_febbraio.substr(0, OrariUrl_febbraio.length - 11) + links_from_file[classe]);
         await page.screenshot({ path: `${classe}.png` });
         await page.waitForTimeout(300)
     }
@@ -169,10 +152,34 @@ async function DownloadIMages() {
 }
 
 
+async function crop(image, giorno, ora, name, entraDopo) {
+
+
+    try {
+        // console.log('ora: ' + ora);
+        // console.log('giorno: ' + giorno);
+        let newImage = await Jimp.read(image)
+
+        if (entraDopo) { newImage = await newImage.crop(62 + 118 * giorno, 53 + 67 * ora, 90, 61) } else { newImage = await newImage.crop(62 + 118 * giorno, (ora == 6 ? 73 : 72) + 67 * ora, 90, 61) }
+
+        newImage.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+            fs.writeFileSync('test/' + ` ora: ${giorno}- giorno: ${ora} ` + name, buffer)
+        })
+        return newImage;
+    } catch (error) {
+        throw ('ERRORE IN CROP(): ' + error)
+    }
+
+}
+
+
+
+
+
 async function AnalyzeImagesLocally() {
 
-    console.log('downloading images. . . ');
-    await DownloadIMages();
+    // console.log('downloading images. . . ');
+    // await DownloadIMages();
     let url = await getLinks();
     // let url = JSON.parse(fs.readFileSync('links.json', 'utf8'));
 
@@ -183,32 +190,47 @@ async function AnalyzeImagesLocally() {
     const dir = fs.opendirSync('.')
     let dirent
     while ((dirent = dir.readSync()) !== null) {
-        if (dirent.name.includes('png')) {
+        if (dirent.name.includes('.png')) {
             console.log(dirent.name)
             let orari = [];
             let ita = 0;
             var image = await Jimp.read(dirent.name)
 
-            image.resize(800, 600)
+            image.resize(800, 600) // FIXME da fare il crop a seconda del se entra alla 3à ora o meno 
             let totaleGiorni = 6;
+            let ore = 6;
+            let entraDopo = [];
             let doesSabato = [];
+
             for (let sabato = 0; sabato < 5; sabato++) {
-                let materia = image.getPixelColor(65 + 113 * 5, 78 + 78 * sabato)
+                let materia = image.getPixelColor(65 + 113 * 6, 78 + 78 * sabato)
                 doesSabato.push(colors[materia])
             }
             if (doesSabato.every((val, i, arr) => val === arr[0])) totaleGiorni = 5;
 
-            for (let giorno = 0; giorno < 6; giorno++) {
-                // console.log(`ora numero: ${giorno}`);
-                for (let ora = 0; ora < totaleGiorni; ora++) {
+            for (let primaOra = 0; primaOra < 7; primaOra++) {
+                let materia = image.getPixelColor(96 + 113 * primaOra, 104)
+                entraDopo.push(colors[materia])
+            }
+            if (entraDopo.every((val, i, arr) => val === arr[0])) ore = 5;
 
-                    var rgb = image.getPixelColor(65 + 118 * ora, 78 + 78 * giorno);
+            if (entraDopo.every((val, i, arr) => val === arr[0])) image = await image.crop(0, 154, 800, 600 - 154);
+
+            // image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+            //     fs.writeFileSync('cropped/' + dirent.name, buffer)
+            // })
+
+            for (let ora = 0; ora < ore; ora++) { //! sono 5 le ore su cui loopare perche ci sono le lezioni ridotte per covid se no sarebbero 6 
+                // console.log(`ora numero: ${giorno}`);
+                for (let giorno = 0; giorno < totaleGiorni; giorno++) {
+
+                    var rgb = image.getPixelColor(65 + 118 * giorno, 70 + 75 * ora);
                     //if(rgb==null) rgb = image.getPixelColor(65+118*ora, 78+78*giorno+15) 3E giorno=5 ora=2 scienze motorie && 1L giorno=5 ora=3 scienze motorie && 3F giorno=5 ora=0 scienze motorie && 1N giorno=5 ora=1 scienze motorie && 
-                    if (colors[rgb] == null) rgb = image.getPixelColor(65 + 118 * ora, 78 + 78 * giorno + 20);
+                    if (colors[rgb] == null) rgb = image.getPixelColor(65 + 118 * giorno, 70 + 75 * ora + 20);
                     if (colors[rgb] == null) console.log('\n' + 'ALERT' + '\n');
                     if (colors[rgb] == 'ITA') ita++;
                     if (colors[rgb] == 'ITA' && ita >= 3) { //get array of pixels of rectangle
-                        let imageCrop = await crop(image, ora, giorno, dirent.name)
+                        let imageCrop = await crop(image, giorno, ora, dirent.name, ore == 5)
                         let rgbArray = [];
                         for (let y = 0; y < imageCrop.bitmap.height; y++) {
                             for (let x = 0; x < imageCrop.bitmap.width; x++) {
@@ -219,16 +241,17 @@ async function AnalyzeImagesLocally() {
                         if (rgbArray.every((val, i, arr) => val === arr[0])) orari.push('')
                         else orari.push('ITA')
                     } else orari.push(colors[rgb])
-                        //console.log([colors[rgb], Jimp.intToRGBA(rgb)]);
+                        // console.log([colors[rgb], Jimp.intToRGBA(rgb)]);
                         // let croppedImage = image.crop(76 + 152 * giorno, 90 + 100 * ora - (ora != 0 ? 15 : 0), 152, 130 - (ora != 0 && ora != 5 ? 0 : 15))
                         // croppedImage.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
                         //     fs.writeFileSync(__dirname + '/_giorno_' + giorno.toString() + '_ore_' + ora.toString() + '.png', buffer)
+                        // })
 
 
                 }
                 if (totaleGiorni == 5) orari.push('')
             }
-
+            if (ore == 5) orari = ['', '', '', '', '', '', '', '', '', '', '', '', ...orari]
             orariJson[dirent.name.split('.')[0]] = {}
             orariJson[dirent.name.split('.')[0]][`url`] = url[dirent.name.split('.')[0]];
             orariJson[dirent.name.split('.')[0]][`${dirent.name.split('.')[0]}`] = orari;
@@ -257,77 +280,100 @@ async function AnalyzeImagesOnline() {
     let links_from_file = await getLinks()
     console.log('analyzing images. . . ');
     for (let classe in links_from_file) {
+        if (classe == '1G') {
 
-        console.log(classe);
-
-
-
-        let orari = [];
-        let ita = 0;
-        let url = 'https://www.messedaglia.edu.it/images/stories/2020-21/orario/' + links_from_file[classe]
-        const image = await Jimp.read(url)
-        await image.resize(800, 600)
-
-        let totaleGiorni = 6;
-        let doesSabato = [];
-        for (let sabato = 0; sabato < 5; sabato++) {
-            let materia = image.getPixelColor(65 + 113 * 6, 78 + 78 * sabato)
-            doesSabato.push(colors[materia])
-        }
-        if (doesSabato.every((val, i, arr) => val === arr[0])) totaleGiorni = 5;
-
-        for (let giorno = 0; giorno < 6; giorno++) {
-            // console.log(`ora numero: ${giorno}`);
-            for (let ora = 0; ora < totaleGiorni; ora++) {
-
-                var rgb = image.getPixelColor(65 + 118 * ora, 78 + 78 * giorno);
-                //if(rgb==null) rgb = image.getPixelColor(65+118*ora, 78+78*giorno+15) 3E giorno=5 ora=2 scienze motorie && 1L giorno=5 ora=3 scienze motorie && 3F giorno=5 ora=0 scienze motorie && 1N giorno=5 ora=1 scienze motorie && 
-                if (colors[rgb] == null) rgb = image.getPixelColor(65 + 118 * ora, 78 + 78 * giorno + 20);
-                if (colors[rgb] == null) console.log('\n' + 'ALERT' + '\n');
-                if (colors[rgb] == 'ITA') ita++;
-                if (colors[rgb] == 'ITA' && ita >= 3) { //get array of pixels of rectangle
-                    let imageCrop = await crop(image, ora, giorno, classe)
-                    let rgbArray = [];
-                    for (let y = 0; y < imageCrop.bitmap.height; y++) {
-                        for (let x = 0; x < imageCrop.bitmap.width; x++) {
-                            let color = imageCrop.getPixelColor(x, y);
-                            rgbArray.push(color)
-                        }
-                    }
-                    if (rgbArray.every((val, i, arr) => val === arr[0])) orari.push('')
-                    else orari.push('ITA')
-                } else orari.push(colors[rgb])
-                    //console.log([colors[rgb], Jimp.intToRGBA(rgb)]);
-                    // let croppedImage = image.crop(76 + 152 * giorno, 90 + 100 * ora - (ora != 0 ? 15 : 0), 152, 130 - (ora != 0 && ora != 5 ? 0 : 15))
-                    // croppedImage.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-                    //     fs.writeFileSync(__dirname + '/_giorno_' + giorno.toString() + '_ore_' + ora.toString() + '.png', buffer)
+            console.log(classe);
 
 
+
+            let orari = [];
+            let ita = 0;
+            let url = OrariUrl_febbraio.substr(0, OrariUrl_febbraio.length - 11) + links_from_file[classe]
+            var image = await Jimp.read(url)
+            await image.resize(800, 600)
+
+            let totaleGiorni = 6;
+            let ore = 6;
+            let entraDopo = [];
+            let doesSabato = [];
+            for (let sabato = 0; sabato < 5; sabato++) {
+                let materia = image.getPixelColor(65 + 113 * 6, 78 + 78 * sabato)
+                doesSabato.push(colors[materia])
             }
-            if (totaleGiorni == 5) orari.push('')
+            if (doesSabato.every((val, i, arr) => val === arr[0])) totaleGiorni = 5;
+
+            for (let primaOra = 0; primaOra < 7; primaOra++) {
+                let materia = image.getPixelColor(96 + 113 * primaOra, 104)
+                entraDopo.push(colors[materia])
+            }
+            if (entraDopo.every((val, i, arr) => val === arr[0])) ore = 5;
+
+            if (entraDopo.every((val, i, arr) => val === arr[0])) image = await image.crop(0, 154, 800, 600 - 154);
+
+            image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+                fs.writeFileSync('cropped/' + classe + '.png', buffer)
+            })
+
+            for (let ora = 0; ora < ore; ora++) { //! sono 5 le ore su cui loopare perche ci sono le lezioni ridotte per covid se no sarebbero 6 
+                // console.log(`ora numero: ${giorno}`);
+                for (let giorno = 0; giorno < totaleGiorni; giorno++) {
+
+                    var rgb = image.getPixelColor(65 + 118 * giorno, 70 + 75 * ora);
+                    //if(rgb==null) rgb = image.getPixelColor(65+118*ora, 78+78*giorno+15) 3E giorno=5 ora=2 scienze motorie && 1L giorno=5 ora=3 scienze motorie && 3F giorno=5 ora=0 scienze motorie && 1N giorno=5 ora=1 scienze motorie && 
+                    if (colors[rgb] == null) rgb = image.getPixelColor(65 + 118 * giorno, 70 + 75 * ora + 20);
+                    if (colors[rgb] == null) console.log('\n' + 'ALERT' + '\n');
+                    if (colors[rgb] == 'ITA') ita++;
+                    if (colors[rgb] == 'ITA' && ita >= 3) { //get array of pixels of rectangle
+                        let imageCrop = await crop(image, giorno, ora, classe + '.png', ore == 5)
+                        let rgbArray = [];
+                        for (let y = 0; y < imageCrop.bitmap.height; y++) {
+                            for (let x = 0; x < imageCrop.bitmap.width; x++) {
+                                let color = imageCrop.getPixelColor(x, y);
+                                rgbArray.push(color)
+                            }
+                        }
+                        if (rgbArray.every((val, i, arr) => val === arr[0])) orari.push('')
+                        else orari.push('ITA')
+                    } else orari.push(colors[rgb])
+                        // console.log([colors[rgb], Jimp.intToRGBA(rgb)]);
+                        // let croppedImage = image.crop(76 + 152 * giorno, 90 + 100 * ora - (ora != 0 ? 15 : 0), 152, 130 - (ora != 0 && ora != 5 ? 0 : 15))
+                        // croppedImage.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+                        //     fs.writeFileSync(__dirname + '/_giorno_' + giorno.toString() + '_ore_' + ora.toString() + '.png', buffer)
+                        // })
+
+
+                }
+                if (totaleGiorni == 5) orari.push('')
+            }
+
+            if (ore == 5) orari = ['', '', '', '', '', '', '', '', '', '', '', '', ...orari]
+            orariJson[classe] = {}
+            orariJson[classe]['url'] = url;
+            orariJson[classe][`${classe}`] = orari;
+
+
+
+
+
+
+
         }
-
-        orariJson[classe] = {}
-        orariJson[classe]['url'] = url;
-        orariJson[classe][`${classe}`] = orari;
-
-
-
-
-
-
-
     }
+
 
 }
 
+
 async function start() {
-    //await getLinks()
-    // await getImages()
+    // await getLinks()
     // await DownloadIMages()
     // await AnalyzeImagesLocally()
     await AnalyzeImagesOnline()
-    console.log(orariJson);
+        // console.log(orariJson);
+
+    const sortObject = obj => Object.keys(obj).sort().reduce((res, key) => (res[key] = obj[key], res), {})
+    let arr = sortObject(orariJson);
+    console.log(arr);
     // console.log(orariJson)
     // fs.writeFileSync('orari.json', JSON.stringify(orariJson))
 
